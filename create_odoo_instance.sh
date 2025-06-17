e5dad4af14cfedc4559836400b3dc3028312f591
musardotiago <musardotiago@gmail.com>
2025-06-16 22:07:52 -0300

    Update create_odoo_instance.sh

--- create_odoo_instance.sh
+++ create_odoo_instance.sh
@@ -1,77 +1,70 @@
 #!/bin/bash
 
-# Parâmetros obrigatórios
-CLIENT_NAME=$1
-SUBDOMAIN=$2
-DB_PASSWORD=$3
-ADMIN_PASSWD=$4
+set -e
 
-if [[ -z "$CLIENT_NAME" || -z "$SUBDOMAIN" || -z "$DB_PASSWORD" || -z "$ADMIN_PASSWD" ]]; then
-  echo "Uso: $0 <nome_cliente> <subdominio> <db_password> <admin_password>"
+CLIENT=$1
+DOMAIN="$CLIENT.mitsd.com.br"
+BASE_DIR="/srv/odoo/clients/$CLIENT"
+PORT=$((RANDOM%1000+9000))  # Ex: porta interna do container
+ODOO_CONF="$BASE_DIR/config/odoo.conf"
+
+if [ -z "$CLIENT" ]; then
+  echo "Usage: $0 <client-name>"
   exit 1
 fi
 
-# Caminhos
-BASE_DIR="/srv/odoo"
-CLIENT_DIR="$BASE_DIR/clients/$CLIENT_NAME"
-TEMPLATE_CONF="$BASE_DIR/base/odoo.conf.template"
-CLIENT_CONF="$CLIENT_DIR/odoo.conf"
+# Criar estrutura de pastas
+mkdir -p "$BASE_DIR"/{data,config,addons,logs}
+cp /srv/odoo/base/odoo.conf "$ODOO_CONF"
+sed -i "s/db_name = .*/db_name = $CLIENT/" "$ODOO_CONF"
+sed -i "s|addons_path = .*|addons_path = /mnt/extra-addons,/usr/lib/python3/dist-packages/odoo/addons|" "$ODOO_CONF"
+sed -i "s|logfile = .*|logfile = /var/log/odoo/odoo.log|" "$ODOO_CONF"
 
-# 1. Criar estrutura
-mkdir -p "$CLIENT_DIR/custom-addons" "$CLIENT_DIR/filestore" "$CLIENT_DIR/log"
-chown -R ubuntu:ubuntu "$CLIENT_DIR"
-
-# 2. Gerar .env
-cat <<EOF > "$CLIENT_DIR/.env"
-CLIENT_NAME=$CLIENT_NAME
-SUBDOMAIN=$SUBDOMAIN
-DB_NAME=$CLIENT_NAME
-DB_USER=odoo
-DB_PASSWORD=$DB_PASSWORD
-ADMIN_PASSWD=$ADMIN_PASSWD
+# Criar .env
+cat > "$BASE_DIR/.env" <<EOF
+ODOO_DB=$CLIENT
+ODOO_PORT=$PORT
 EOF
 
-# 3. Gerar odoo.conf
-cp "$TEMPLATE_CONF" "$CLIENT_CONF"
-sed -i "s|\${DB_PASSWORD}|$DB_PASSWORD|g" "$CLIENT_CONF"
-sed -i "s|\${ADMIN_PASSWD}|$ADMIN_PASSWD|g" "$CLIENT_CONF"
-
-# 4. docker-compose.yml (com env_file)
-cat <<EOF > "$CLIENT_DIR/docker-compose.yml"
-version: '3.1'
+# Criar docker-compose.yml por cliente
+cat > "$BASE_DIR/docker-compose.yml" <<EOF
+version: "3.9"
 
 services:
-  db:
-    image: postgres:13
-    env_file:
-      - .env
-    environment:
-      POSTGRES_DB=\${DB_NAME}
-      POSTGRES_USER=\${DB_USER}
-      POSTGRES_PASSWORD=\${DB_PASSWORD}
-    volumes:
-      - ./filestore:/var/lib/postgresql/data
-    restart: always
-
   odoo:
     image: odoo:17
+    container_name: odoo_$CLIENT
     env_file:
       - .env
-    depends_on:
-      - db
     volumes:
-      - ./custom-addons:/opt/odoo/custom-addons
-      - ./odoo.conf:/etc/odoo/odoo.conf
-      - ./log:/var/log/odoo
-    labels:
-      - "traefik.enable=true"
-      - "traefik.http.routers.\${CLIENT_NAME}.rule=Host(\`\${SUBDOMAIN}\`)"
-      - "traefik.http.services.\${CLIENT_NAME}.loadbalancer.server.port=8069"
+      - ./data:/var/lib/odoo
+      - ./config/odoo.conf:/etc/odoo/odoo.conf
+      - ./addons:/mnt/extra-addons
     restart: always
+    expose:
+      - "8069"
 EOF
 
-chown -R ubuntu:ubuntu "$CLIENT_DIR"
+# Criar config NGINX para o subdomínio
+NGINX_CONF="/srv/odoo/docker/nginx/sites-enabled/$CLIENT.conf"
+cat > "$NGINX_CONF" <<EOF
+server {
+    listen 80;
+    server_name $DOMAIN;
 
-echo "Instância '$CLIENT_NAME' criada com sucesso."
-echo "Subdomínio: $SUBDOMAIN"
-echo "Para subir: cd $CLIENT_DIR && docker compose up -d"
+    location / {
+        proxy_pass http://odoo_$CLIENT:8069;
+        proxy_set_header Host \$host;
+        proxy_set_header X-Real-IP \$remote_addr;
+        proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+    }
+
+    location /longpolling {
+        proxy_pass http://odoo_$CLIENT:8072;
+    }
+}
+EOF
+
+echo "[INFO] Cliente '$CLIENT' configurado. Subdomínio: http://$DOMAIN"
+echo "[INFO] Lembre-se de adicionar '$DOMAIN' ao DNS apontando para o IP do servidor."
